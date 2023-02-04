924a937c57f56b81d0e2fca171e852641e605a97
omidhoseini <webbmakerr@gmail.com>
2023-02-05 01:27:04 +0330

    #1 Add customers' table

---
+++ database/migrations/2023_02_04_120502_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname', 255);
+            $table->string('lastname', 255);
+            $table->string('phone_number', 255)->nullable();
+            $table->string('email', 255);
+            $table->string('password', 255);
+            $table->string('is_suspended')->default('active');
+            $table->timestamp('last_login')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
