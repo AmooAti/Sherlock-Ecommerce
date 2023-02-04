7b754d47f01f33a812e9fc35eb6a68411fe4a420
omidhoseini <webbmakerr@gmail.com>
2023-02-05 01:32:02 +0330

    #1 Add customer factory

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'firstname'    => fake()->firstName(),
+            'lastname'     => fake()->lastName(),
+            'phone_number' => fake()->phoneNumber(),
+            'email'        => fake()->safeEmail(),
+            'password'     => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
+        ];
+    }
+}
