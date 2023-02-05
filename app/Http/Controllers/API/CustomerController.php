17b74cef02c71f3c72e9a6362eeee1e68c1c7937
omidhoseini <webbmakerr@gmail.com>
2023-02-05 11:27:28 +0330

    #1 Improve register function,

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -7,7 +7,6 @@ use App\Http\Requests\API\RegisterCustomerRequest;
 use App\Models\Customer;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Hash;
-use RuntimeException;
 
 class CustomerController extends Controller
 {
@@ -20,24 +19,18 @@ class CustomerController extends Controller
      */
     public function register(RegisterCustomerRequest $request): JsonResponse
     {
-        try {
-            $request->validated($request->all());
+        $request->validated();
 
-            $customer = Customer::factory()->createOne([
-                'firstname'    => $request->get('firstname'),
-                'lastname'     => $request->get('lastname'),
-                'phone_number' => $request->get('phone_number'),
-                'email'        => $request->get('email'),
-                'password'     => Hash::make($request->get('password')),
-            ]);
+        $customer = Customer::create([
+            'firstname'    => $request->get('firstname'),
+            'lastname'     => $request->get('lastname'),
+            'phone_number' => $request->get('phone_number'),
+            'email'        => $request->get('email'),
+            'password'     => Hash::make($request->get('password')),
+        ]);
 
-            return response()
-                ->json(['message' => "The customer (#$customer->id) is created successfully."])
-                ->setStatusCode(201);
-        } catch (RuntimeException $exception) {
-            return response()
-                ->json(['message' => $exception->getMessage()])
-                ->setStatusCode($exception->getCode());
-        }
+        return response()
+            ->json(['message' => "The customer (#$customer->id) is created successfully."])
+            ->setStatusCode(201);
     }
 }
