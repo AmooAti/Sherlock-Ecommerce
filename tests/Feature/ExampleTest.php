c34b5f7557e50ea8c03c7a1b7913754e173edd9b
adhamiamirhossein <adhamiamirhossein@gmail.com>
2023-02-06 00:08:20 +0330

    removed - main test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,10 +12,5 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
 }
