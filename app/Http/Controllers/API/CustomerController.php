8d0931e17780a2c4d93e24fd422163a08bf94551
m.eslampanah18 <m.eslampanah18@gmail.com>
2023-02-08 01:50:23 +0330

    #2 add login route and controller with request and res

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\CustomerController\LoginRequest;
 use App\Http\Requests\API\RegisterCustomerRequest;
+use App\Http\Resources\LoginResource;
 use App\Models\Customer;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class CustomerController extends Controller
@@ -32,5 +36,20 @@ class CustomerController extends Controller
         return response()
             ->json(['message' => "The customer (#$customer->id) is created successfully."])
             ->setStatusCode(201);
+    }
+
+    public  function login(LoginRequest $request)
+    {
+
+        $credentials =   $request->validated();
+        $customer = Customer::where('email', $credentials["email"])->first();
+
+        if (!$customer || ! Hash::check($credentials["password"], $customer->password)) {
+
+            return response()->json(['error' => 'The provided credentials are incorrect.'], 401);
+
+        }
+        $token = $customer->createToken('api_token');
+        return LoginResource::make($token);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,6 @@ use App\Http\Controllers\API\CustomerController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/customer/register', [CustomerController::class, 'register'])->name('customer.register');
+Route::post('/customer/login', [CustomerController::class, 'login'])
+    ->name('customer.login');
+
