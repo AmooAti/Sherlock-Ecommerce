a27edfa3bc00cd61113d99e0221d29be1e6d6cd6
omidhoseini <webbmakerr@gmail.com>
2023-02-10 16:44:13 +0330

    #1 Added admin customer-create route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,16 @@
 <?php
 
-use App\Http\Controllers\API\CustomerController;
+use App\Http\Controllers\API\Admin\CustomerController as AdminCustomer;
+use App\Http\Controllers\API\Customer\CustomerController;
 use Illuminate\Support\Facades\Route;
 
+// Admin action routes.
+Route::name('admin.')
+    // ->middleware(['auth:sanctum'])
+    ->controller(AdminCustomer::class)
+    ->group(function () {
+        Route::post('/admin/customer', 'store')->name('customer.store');
+    });
+
+// Customer action routes.
 Route::post('/customer/register', [CustomerController::class, 'register'])->name('customer.register');
