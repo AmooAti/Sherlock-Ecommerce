7826035ed94e0e550e0cdc19e41c393b9e8c006d
m.eslampanah18 <m.eslampanah18@gmail.com>
2023-02-12 19:47:32 +0330

    #4 add create admin command

---
+++ app/Console/Commands/Admin/CreateAdmin.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands\Admin;
+
+use App\Models\Admin;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class CreateAdmin extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:admin {email} {password?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+
+
+
+        if(is_null($password)){
+            $password = Str::random(8);
+        }
+
+        Admin::create([
+            'email'        => $email,
+            'password'     => $password,
+        ]);
+        echo "Finished";
+        return Command::SUCCESS;
+
+    }
+
+
+
+
+
+}
