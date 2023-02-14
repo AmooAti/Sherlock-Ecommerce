9747f23d077140bb189b18c7859336df2055ab36
omidhoseini <webbmakerr@gmail.com>
2023-02-14 22:33:47 +0330

    #9 - Added admin-customer-edit tests

--- tests/Feature/Http/Controllers/API/Admin/CustomerControllerTest.php
+++ tests/Feature/Http/Controllers/API/Admin/CustomerControllerTest.php
@@ -14,6 +14,239 @@ class CustomerControllerTest extends TestCase
 
     const ADMIN_CUSTOMER_STORE = 'admin.customer.store';
     const ADMIN_CUSTOMER_INDEX = 'admin.customer.index';
+    const ADMIN_CUSTOMER_UPDATE = 'admin.customer.update';
+
+    /**
+     * Test as an admin, It should not edit a customer with a password without lowercase.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_not_edit_a_customer_with_a_password_without_lowercase(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'password' => strtoupper($this->faker->password(8)),
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertInvalid('password');
+    }
+
+    /**
+     * Test as an admin, It should not edit a customer with a password without uppercase.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_not_edit_a_customer_with_a_password_without_uppercase(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'password' => strtolower($this->faker->password(8)),
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertInvalid('password');
+    }
+
+    /**
+     * Test as an admin, It should not edit a customer with a password that has not a number.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_not_edit_a_customer_with_a_password_that_has_not_a_number(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'password' => 'Password',
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertInvalid('password');
+    }
+
+    /**
+     * Test as an admin, It should not edit a customer with a password that is less than 8 characters.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_not_edit_a_customer_with_a_password_that_is_less_than_8_characters(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'password' => $this->faker->password(6, 7),
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertInvalid('password');
+    }
+
+    /**
+     * Test as an admin, It should not edit a customer with a duplicate email.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_not_edit_a_customer_with_a_duplicate_email(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'email' => $customer->getAttribute('email'),
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertInvalid('email');
+    }
+
+    /**
+     * Test as an admin, It should not edit a customer with an invalid email.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_not_edit_a_customer_with_an_invalid_email(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'email' => $this->faker->name,
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertInvalid('email');
+    }
+
+    /**
+     * Test as an admin, it should not edit a customer with invalid inputs.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_not_edit_a_customer_with_invalid_inputs(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'firstname'    => $this->faker->numberBetween(4, 12),
+            'lastname'     => $this->faker->numberBetween(4, 12),
+            'phone_number' => $this->faker->numberBetween(8, 11),
+            'is_suspended' => $this->faker->numberBetween(4, 12),
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertInvalid(['firstname', 'lastname', 'phone_number', 'is_suspended']);
+    }
+
+    /**
+     * Test as an admin, it should edit a customer with valid inputs.
+     *
+     * @return void
+     */
+    public function test_as_an_admin_it_should_edit_a_customer_with_valid_inputs(): void
+    {
+        $customer = Customer::factory()->createOne(
+            [
+                'firstname'    => $this->faker->firstName,
+                'lastname'     => $this->faker->lastName,
+                'email'        => $this->faker->safeEmail,
+                'password'     => Hash::make($this->faker->password(8)),
+                'phone_number' => $this->faker->phoneNumber,
+                'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+            ]
+        );
+
+        $customerID = $customer->getAttribute('id');
+
+        $payload = [
+            'firstname'    => $this->faker->firstName,
+            'lastname'     => $this->faker->lastName,
+            'email'        => $this->faker->safeEmail,
+            'password'     => $this->faker->password(8),
+            'phone_number' => $this->faker->phoneNumber,
+            'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
+        ];
+
+        $this->putJson(route(self::ADMIN_CUSTOMER_UPDATE, [$customerID]), $payload)
+            ->assertSuccessful();
+    }
 
     /**
      * Test as an admin, it should get the count of customers equal to the limit variable exactly
@@ -146,7 +379,7 @@ class CustomerControllerTest extends TestCase
             'firstname'    => $this->faker->firstName(),
             'lastname'     => $this->faker->lastName(),
             'email'        => $customer->getAttribute('email'),
-            'password'     => Hash::make($this->faker->password(8)),
+            'password'     => $this->faker->password(8),
             'phone_number' => $this->faker->phoneNumber(),
             'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
         ];
@@ -166,7 +399,7 @@ class CustomerControllerTest extends TestCase
             'firstname'    => $this->faker->firstName,
             'lastname'     => $this->faker->lastName,
             'email'        => $this->faker->name,
-            'password'     => Hash::make($this->faker->password(8)),
+            'password'     => $this->faker->password(8),
             'phone_number' => $this->faker->phoneNumber,
             'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
         ];
@@ -176,11 +409,11 @@ class CustomerControllerTest extends TestCase
     }
 
     /**
-     * Test as an admin, it should not create a customer without required parameters.
+     * Test as an admin, it should not create a customer without required inputs.
      *
      * @return void
      */
-    public function test_as_an_admin_it_should_not_create_a_customer_without_required_parameters(): void
+    public function test_as_an_admin_it_should_not_create_a_customer_without_required_inputs(): void
     {
         $payload = [
             'firstname'    => null,
@@ -196,19 +429,19 @@ class CustomerControllerTest extends TestCase
     }
 
     /**
-     * Test as an admin, it should not create a customer with invalid parameters.
+     * Test as an admin, it should not create a customer with invalid inputs.
      *
      * @return void
      */
-    public function test_as_an_admin_it_should_create_a_customer_with_invalid_parameters(): void
+    public function test_as_an_admin_it_should_create_a_customer_with_invalid_inputs(): void
     {
         $payload = [
-            'firstname'    => rand(4, 12),
-            'lastname'     => rand(4, 12),
+            'firstname'    => $this->faker->numberBetween(4, 12),
+            'lastname'     => $this->faker->numberBetween(4, 12),
             'email'        => $this->faker->name,
             'password'     => $this->faker->password(6, 7),
-            'phone_number' => rand(8, 11),
-            'is_suspended' => rand(4, 12),
+            'phone_number' => $this->faker->numberBetween(8, 11),
+            'is_suspended' => $this->faker->numberBetween(4, 12),
         ];
 
         $this->postJson(route(self::ADMIN_CUSTOMER_STORE), $payload)
@@ -216,17 +449,17 @@ class CustomerControllerTest extends TestCase
     }
 
     /**
-     * Test as an admin, it should create a customer with valid parameters and insert to database
+     * Test as an admin, it should create a customer with valid inputs and insert to database
      *
      * @return void
      */
-    public function test_as_an_admin_it_should_create_a_customer_with_valid_parameters_and_insert_to_database(): void
+    public function test_as_an_admin_it_should_create_a_customer_with_valid_inputs_and_insert_to_database(): void
     {
         $payload = [
             'firstname'    => $this->faker->firstName,
             'lastname'     => $this->faker->lastName,
             'email'        => $this->faker->unique()->safeEmail,
-            'password'     => Hash::make($this->faker->password(8)),
+            'password'     => $this->faker->password(8),
             'phone_number' => $this->faker->phoneNumber,
             'is_suspended' => $this->faker->randomElement(['active', 'deactivate']),
         ];
