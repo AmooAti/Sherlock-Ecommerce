23a4c739bd086d960de03a0f396752351401c1f8
omidhoseini <webbmakerr@gmail.com>
2023-02-21 00:54:47 +0330

    #5 - Used the `Authenticatable` replace `Model`, added the `$hidden` and the `$casts` properties.

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -3,18 +3,43 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'firstname',
         'lastname',
         'email',
         'password',
         'phone_number',
-        'status'
+        'status',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<int, string>
+     */
+    protected $casts = [
+        'last_login' => 'datetime',
     ];
 }
